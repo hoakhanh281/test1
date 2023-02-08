6b98cbe926ef22e14fc7465bfc5979d88f5e613e
hoakhanh281 <118725084+hoakhanh281@users.noreply.github.com>
2023-02-08 19:11:39 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,23 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.8"
+      version = "~> 4.16"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
+
 
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-00874d747dde814fa" # Ubuntu 20.04 LTS // us-east-1
+resource "aws_instance" "app_server" {
+  ami = "ami-00874d747dde814fa"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
 }
