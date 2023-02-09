9abcef3d0a7528271ec833d032c0fdf5aca7a823
hoakhanh281 <118725084+hoakhanh281@users.noreply.github.com>
2023-02-09 14:17:59 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 3.0"
     }
   }
 
-  required_version = ">= 1.2.0"
 }
 
 
